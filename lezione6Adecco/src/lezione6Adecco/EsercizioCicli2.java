9e9b1da0f1ee9ca6354b4077eb7c710c2c82bbcd
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2022-11-02 09:45:37 +0100

    Add files via upload

---
+++ lezione6Adecco/src/lezione6Adecco/EsercizioCicli2.java
@@ -0,0 +1,27 @@
+package lezione6Adecco;
+
+/**
+ * 
+ * Creare un programma
+ * che prenda una sequenza di numeri
+ * che termina quando si inserisce
+ * il valore di -1
+ * al termine della sequenz
+ * il programma stampa quanti numeri
+ * pari sono stati inseriti
+ * 
+ * Es :  2 5 10 12 -1 -------->3
+ * 
+ * 
+ * @author mr
+ *
+ */
+
+public class EsercizioCicli2 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+}
