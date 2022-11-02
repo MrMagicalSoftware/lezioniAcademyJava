04f82861d7b5146a4183d100870a2c2ca0f0d432
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2022-11-02 10:36:39 +0100

    Add files via upload

---
+++ lezione6Adecco/src/lezione6Adecco/Esercizio3.java
@@ -0,0 +1,30 @@
+package lezione6Adecco;
+
+
+/**
+ * 
+ * data una sequenza di numeri
+ * che termina quando si inserisce un 
+ * valore maggiore di 20 .
+ * 
+ * al termine della sequenza
+ * il programma stampa se tutti i numeri
+ * inseriti sono multipli di 3
+ * 
+ * 
+ * Es :   3 3 6 12 22 ----> yes
+ *        3 3 5 21 ----> no
+ * 
+ * @author mr
+ *
+ */
+
+
+public class Esercizio3 {
+
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
