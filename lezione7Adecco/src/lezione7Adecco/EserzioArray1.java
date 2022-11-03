616429563ca2c424a266f5e29c9b2d3307001ea9
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2022-11-03 14:34:27 +0100

    Add files via upload

---
+++ lezione7Adecco/src/lezione7Adecco/EserzioArray1.java
@@ -0,0 +1,38 @@
+package lezione7Adecco;
+
+
+/***
+ * 
+ * 
+ * 
+ *  dato un vettore
+ *  gia' inizializz  
+ *  
+ *      int[] vett = { 2 , 4 ,18  }  -----> SI
+ *      
+ *      int[] vett = { 2 , 1 ,18  }  ----> NO
+ *      
+ *      stampare la scritta
+ *      
+ *      "contiene tutti elementi pari"
+ *      se il vettore contiene tutti elementi pari
+ *      
+ *      altrimenti 
+ *      dire che non tutti gli elmenti sono pari
+ * 
+ * 
+ * 		Nota : USARE IL BREAK IN CHECK!!
+ * 
+ * 
+ * @author mr
+ *
+ */
+
+public class EserzioArray1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
