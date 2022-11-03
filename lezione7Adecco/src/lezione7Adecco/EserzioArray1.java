3f3e3f388f525e7eed7c12f0e579b1b96c57f9d9
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2022-11-03 15:11:25 +0100

    Add files via upload

--- lezione7Adecco/src/lezione7Adecco/EserzioArray1.java
+++ lezione7Adecco/src/lezione7Adecco/EserzioArray1.java
@@ -33,6 +33,31 @@ public class EserzioArray1 {
 	public static void main(String[] args) {
 		
 
+		boolean isAllNumberEven = true;
+		
+		
+		int[] vett = {  4 , 20 ,1,  50 , 100 , 77 , 100 , 300 };
+		
+		
+		for (int i = 0; i < vett.length; i++) {
+			
+			System.out.println("sto considerando l'elemnto " + vett[i]  );
+			
+			if(vett[i] % 2 != 0 ) {
+				isAllNumberEven = false;
+				//break;
+			}
+				
+		}
+		
+		
+		if(isAllNumberEven)
+			System.out.println("sono tutti pari");
+		else
+			System.out.println("no");
+		
+		
+		
 	}
 
 }
