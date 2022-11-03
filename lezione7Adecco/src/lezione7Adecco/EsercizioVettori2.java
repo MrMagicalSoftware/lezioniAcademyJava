b8c8ef64ac45cece70a7c298a52eca8348a1b9f7
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2022-11-03 11:41:59 +0100

    Add files via upload

---
+++ lezione7Adecco/src/lezione7Adecco/EsercizioVettori2.java
@@ -0,0 +1,45 @@
+package lezione7Adecco;
+
+
+
+/**
+ * 
+ * 	creare un programma 
+ *  che prenda un numero che sia 
+ *  compreso tra 3  e 10 estremi inclusi
+ *  (semaforo)
+ *  
+ *  il numero che viene inserito mi
+ *  determina la grandezza del vettore.
+ *  
+ *  riempire il vettore con lo scanner
+ *  elemento per elemento
+ *  
+ *  stampare il vettore.
+ * 		
+ * 
+ *  Es grandezza 3
+ *  
+ *         vett[0] <==== utente
+ * 		   vett[1] <==== utente
+ * 		   vett[2] <==== utente
+ * 
+ * 
+ */
+
+
+
+import java.util.Scanner;
+public class EsercizioVettori2 {
+
+	public static void main(String[] args) {
+		
+		Scanner key = new Scanner(System.in);
+		
+		
+	}
+	
+	
+	
+	
+}
