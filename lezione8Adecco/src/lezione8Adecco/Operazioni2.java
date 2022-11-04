08ead53d1da056d1bd605f73ec2260f485e9e993
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2022-11-04 17:08:15 +0100

    Add files via upload

--- lezione8Adecco/src/lezione8Adecco/Operazioni2.java
+++ lezione8Adecco/src/lezione8Adecco/Operazioni2.java
@@ -19,14 +19,34 @@ package lezione8Adecco;
  * 
  * 
  * 
- * 
- * 
- * 
- * 
  * @author mr
  *
  */
 
 public class Operazioni2 {
 
+	
+	public boolean isEven(int numero) {
+		return numero % 2 == 0;
+	}
+	
+	
+	public boolean isWordStartWithVowel(String parola) {
+		
+		char firstCharacter = parola.charAt(0);
+		
+		if(firstCharacter == 'a' || 
+				firstCharacter == 'e' ||
+				firstCharacter == 'i' ||
+				firstCharacter == 'o' ||
+				firstCharacter == 'u')
+			return true;
+		
+		return false;
+	}
+	
+	
+	
+	
+	
 }
