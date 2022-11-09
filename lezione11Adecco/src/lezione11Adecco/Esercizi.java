a97e80e2fa47520a9576be8e38c70d6281a12a28
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2022-11-09 09:42:55 +0100

    Add files via upload

---
+++ lezione11Adecco/src/lezione11Adecco/Esercizi.java
@@ -0,0 +1,73 @@
+package lezione11Adecco;
+
+/***
+ * 
+ * 1 - Creare una funzione che 
+ * 	   prenda in input un vettore
+ *     di interi e ritorna il numero
+ *     di elementi dispari.
+ *     
+ *     Es  ([10 , 5 , 15 , 7 ) ------> 3
+ * 
+ * 
+ * 2 - Creare una funzione che 
+ *     prende in input un vettore
+ *     di interi e un numero
+ *     e ritorna un intero
+ *     che rappresenta quanti numeri
+ *     sono maggiori di quel numero
+ *     
+ *     Es ( [10 , 4 , 6 , 18 ] , 12 ) ----> 3
+ * 
+ * 
+ * 
+ * 3 - Creare una funzione che prenda
+ *     un numero intero e ritorna 
+ *     un numero che rappresenta tutti i suoi 
+ *     divisori  
+ *     
+ *     Es ( 10 ) ----> 4
+ *     i divisori di 10 sono 1 - 2 - 5 - 10
+ * 
+ * 
+ * 4- Creare una funzione che prenda in ingresso
+ * 	  un vettore di stringhe e ritorna un vettore
+ *    di stringhe salvando nel vettore solo le stringhe+
+ *    che hanno >= 3 lunghezza.
+ *    
+ *    
+ *    
+ *    (["ciao", "mi" , "he" , "hello"]) -----------> (out---> ["ciao", "hello"])
+ * 
+ * 
+ * 
+ * 
+ * @author mr
+ *
+ */
+
+
+
+
+
+public class Esercizi {
+	
+	
+	public int somma(int n1 , int n2) {
+		return n1+ n2;
+	}
+	
+	
+	
+	
+//	public int sommaElementiVett(int[] vett) {
+//		
+//	}
+//	
+	
+	
+	
+	
+	
+
+}
