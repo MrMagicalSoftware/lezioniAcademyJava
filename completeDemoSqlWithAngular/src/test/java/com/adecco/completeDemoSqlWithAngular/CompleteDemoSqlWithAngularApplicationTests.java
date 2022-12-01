b51da0d84450e356b064c2276001edf85f78e228
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2022-12-01 17:09:04 +0100

    Add files via upload

---
+++ completeDemoSqlWithAngular/src/test/java/com/adecco/completeDemoSqlWithAngular/CompleteDemoSqlWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adecco.completeDemoSqlWithAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteDemoSqlWithAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
